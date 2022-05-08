2af9c9e2f8172e484d4269df094ba410d13e3cbd
Mohamed Amn <mamn2@illinois.edu>
2022-05-09 03:06:36 +0530

    tensorflow gpu usage

---
+++ utils/recurrent_controller2.py
@@ -0,0 +1,47 @@
+import tensorflow.compat.v1 as tf
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+    raise SystemError("GPU not found")
+print('Found GPU at: {}'.format(device_name))
+tf.disable_v2_behavior()
+from controller2 import BaseController
+
+class RecurrentController(BaseController):
+
+    def network_vars(self):
+        print('--define core rnn controller variables--')
+        self.lstm_cell = tf.nn.rnn_cell.LSTMCell(self.hidden_dim)
+        self.state = tf.Variable(tf.zeros([self.batch_size, self.hidden_dim]), trainable=False)
+        self.output = tf.Variable(tf.zeros([self.batch_size, self.hidden_dim]), trainable=False)
+
+    def network_op(self, X, state):
+        print('--operation rnn controller variables--')
+        X = tf.convert_to_tensor(X)
+        return self.lstm_cell(X, state) #DO LSTM computation here, return hidden and the state value
+
+    def update_state(self, new_state):
+        return tf.group(
+            self.output.assign(new_state[0]),
+            self.state.assign(new_state[1])
+        )
+
+    def get_state(self):
+        return self.output, self.state
+
+
+class StatelessRecurrentController(BaseController):
+    def network_vars(self):
+        print('--define core rnn stateless controller variables--')
+        self.lstm_cell = tf.nn.rnn_cell.LSTMCell(self.hidden_dim)
+        self.state = self.lstm_cell.zero_state(self.batch_size, tf.float32)
+
+    def network_op(self, X, state):
+        print('--operation rnn stateless controller variables--')
+        X = tf.convert_to_tensor(X)
+        return self.lstm_cell(X, state)
+
+    def get_state(self):
+        return self.state
+
+    def update_state(self, new_state):
+        return tf.no_op()
